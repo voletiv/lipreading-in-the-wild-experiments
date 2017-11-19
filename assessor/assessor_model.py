70bfba4daf6f09c24000d62fab0da15b55762462
Vikram Voleti <vikky2904@gmail.com>
2017-11-19 21:17:38 +0530

    Trying to make_time_distributed - extra_models, etc

--- assessor/assessor_model.py
+++ assessor/assessor_model.py
@@ -126,17 +126,151 @@ def my_cnn_model(input_shape, conv_f_1, conv_f_2, conv_f_3, cnn_dense_fc_1):
     model.add(Dense(cnn_dense_fc_1, kernel_regularizer=l2(1.e-4)))
     return model
 
-def make_time_distributed(model, TIME_STEPS, input_shape):
+
+def make_time_distributed_simple(model, TIME_STEPS, input_shape):
     time_distributed_model = Sequential()
-    for i in range(len(model.layers)):
-        if i == 0:
-            time_distributed_model.add(TimeDistributed(model.layers[i], input_shape=(TIME_STEPS, *input_shape)))
-        elif not isinstance(model.layers[i], Add):
-            time_distributed_model.add(TimeDistributed(model.layers[i]))
+    for model_layer_index in range(len(model.layers)):
+        print(model_layer_index)
+        if model_layer_index == 0:
+            time_distributed_model.add(TimeDistributed(model.layers[model_layer_index], input_shape=(TIME_STEPS, *input_shape)))
+        elif not isinstance(model.layers[model_layer_index], Add):
+            time_distributed_model.add(TimeDistributed(model.layers[model_layer_index]))
         else:
-            time_distributed_model.add(model.layers[i])
+            print("  Add layer!")
+            time_distributed_model_add_layer_inputs = []
+            # For each add layer input
+            for add_layer_input in model.layers[model_layer_index].input:
+                # Find the add layer input
+                for l in range(model_layer_index):
+                    # print(model.layers[l].output == add_layer_input, model.layers[l].output, add_layer_input)
+                    if model.layers[l].output == add_layer_input:
+                        time_distributed_model_add_layer_inputs.append(time_distributed_model.layers[l].output)
+                        continue
+            # Add them
+            my_added = Add()(time_distributed_model_add_layer_inputs)
+            time_distributed_model = Model(inputs=[time_distributed_model.input], outputs=[my_added])
+    # Return
     return time_distributed_model
 
+
+def make_time_distributed(model, TIME_STEPS, input_shape, verbose=True):
+    extra_models = []
+    extra_time_distributed_models = []
+    model_input = model.input
+    time_distributed_model_input = Input(shape=(TIME_STEPS, *input_shape))
+    # For each layer
+    for model_layer_index in range(1, len(model.layers)):
+        if verbose:
+            print(model_layer_index)
+            print(model.layers[22].output)
+        # If first layer, use time_distributed_model_input as input
+        if model_layer_index == 1:
+            x = TimeDistributed(model.layers[model_layer_index])(time_distributed_model_input)
+            time_distributed_model = Model(inputs=[time_distributed_model_input], outputs=[x])
+        # Else, in case it is not an Add layer,
+        elif not isinstance(model.layers[model_layer_index], Add):
+            # If the previous layer's output is this layer's input, make TimeDistributed
+            if model.layers[model_layer_index-1].output == model.layers[model_layer_index].input:
+                if verbose:
+                    print("Prev layer's output is this layer's input")
+                x = TimeDistributed(model.layers[model_layer_index])(time_distributed_model.layers[model_layer_index-1].output)
+                time_distributed_model = Model(inputs=[time_distributed_model_input], outputs=[x])
+            # Else, (prev layer's output is not this layer's input)
+            else:
+                if verbose:
+                    print("This layer's input is not previous layer's output.")
+                # Find the layer_index whose output is this layer's input, and use that
+                # First search within model
+                if verbose:
+                    print("Searching in prev layers...")
+                found = False
+                for l in range(model_layer_index):
+                    if model.layers[l].output == model.layers[model_layer_index].input:
+                        if verbose:
+                            print("Found at", l)
+                        found = True
+                        break
+                if found == True:
+                    this_model_layer_input = model.layers[l].output
+                    this_time_distributed_layer_input = time_distributed_model.layers[l].output
+                else:
+                    if verbose:
+                        print("Searching in extras...")
+                    for extra_output_index in range(len(extra_models)):
+                        if extra_models[extra_output_index].output == model.layers[model_layer_index].input:
+                            if verbose:
+                                print("Found at", extra_output_index)
+                            this_model_layer_input = extra_models[extra_output_index].output
+                            this_time_distributed_layer_input = extra_time_distributed_models[extra_output_index].output
+                            break
+                # Add this layer's outputs as extra output
+                if verbose:
+                    print("Appending to extra_outputs")
+                y = model.layers[model_layer_index](this_model_layer_input)
+                this_extra_model = Model(inputs=[model_input], outputs=[y])
+                extra_models.append(this_extra_model)
+                z = TimeDistributed(model.layers[model_layer_index])(this_time_distributed_layer_input)
+                this_extra_time_distributed_model = Model(inputs=[time_distributed_model_input], outputs=[z])
+                extra_time_distributed_models.append(this_extra_time_distributed_model)
+                if verbose:
+                    print("extra_models", extra_models)
+                    print("extra_time_distributed_models", extra_time_distributed_models)
+                # # # DONT DO THIS # # #
+                # # Use the found layer input to append time_distributed_model
+                # x = TimeDistributed(model.layers[model_layer_index])(this_layer_input)
+                # time_distributed_model = Model(inputs=[time_distributed_model_input], outputs=[x])
+                # # # DONT DO THIS # # #
+        # ADD layer
+        else:
+            if verbose:
+                print("  Add layer!")
+            time_distributed_model_add_layer_inputs = []
+            # First search within model
+            found = False
+            # For each add layer input
+            for add_layer_input_index, add_layer_input in enumerate(model.layers[model_layer_index].input):
+                # Find the add layer input
+                if verbose:
+                    print("Searching add layer input", add_layer_input_index, "in prev layers...")
+                for l in range(model_layer_index):
+                    # print(model.layers[l].output == add_layer_input, model.layers[l].output, add_layer_input)
+                    if model.layers[l].output == add_layer_input:
+                        if verbose:
+                            print("Found at", l)
+                        found = True
+                        break
+                if found == True:
+                    time_distributed_model_add_layer_inputs.append(time_distributed_model.layers[l].output)
+                # If not found
+                else:
+                    # Search in extras
+                    print("Searching in extras...")
+                    for extra_output_index in range(len(extra_models)):
+                        if extra_models[extra_output_index].output == add_layer_input:
+                            if verbose:
+                                print("Found at", extra_output_index)
+                            time_distributed_model_add_layer_inputs.append(extra_time_distributedmodels[extra_output_index].output)
+                            break
+            # Add them
+            x = Add()(time_distributed_model_add_layer_inputs)
+            time_distributed_model = Model(inputs=[time_distributed_model_input], outputs=[x])
+    # Finally
+    time_distributed_model = Model(inputs=[time_distributed_model_input], outputs=[x])
+    # Return
+    return time_distributed_model
+
+
+# # Check which layer's input has this output
+# for l in range(len(resnet_model.layers)):
+#     if isinstance(resnet_model.layers[l].input, tf.Tensor):
+#             if resnet_model.layers[22].output == resnet_model.layers[l].input:
+#                 l
+#                 break
+#     else:
+#             if resnet_model.layers[22].output in resnet_model.layers[l].input:
+#                 print("multiple")
+#                 l
+#                 break
 
 
 #########################################################
@@ -147,9 +281,9 @@ def make_time_distributed(model, TIME_STEPS, input_shape):
 class CheckpointAndMakePlots(Callback):
 
     # Init
-    def __init__(self, file_name_pre="assessor_cnn_adam", assessor_save_dir="."):
+    def __init__(self, file_name_pre="assessor_cnn_adam", this_assessor_save_dir="."):
         self.file_name_pre = file_name_pre
-        self.assessor_save_dir = assessor_save_dir
+        self.this_assessor_save_dir = this_assessor_save_dir
 
     # On train start
     def on_train_begin(self, logs={}):
@@ -188,22 +322,22 @@ class CheckpointAndMakePlots(Callback):
 
     # Save model checkpoint
     def save_model_checkpoint(self, epoch, tl, ta, vl, va):
-        model_file_path = os.path.join(self.assessor_save_dir,
+        model_file_path = os.path.join(self.this_assessor_save_dir,
             self.file_name_pre + "_epoch{0:03d}_tl{1:.4f}_ta{2:.4f}_vl{3:.4f}_va{4:.4f}.hdf5".format(epoch, tl, ta, vl, va))
         print("Saving model", model_file_path)
         self.model.save_weights(model_file_path)
 
     def save_history(self):
-        print("Saving history in", self.assessor_save_dir)
-        np.savetxt(os.path.join(self.assessor_save_dir, self.file_name_pre + "_loss_history.txt"), self.train_losses, delimiter=",")
-        np.savetxt(os.path.join(self.assessor_save_dir, self.file_name_pre + "_acc_history.txt"), self.train_accuracies, delimiter=",")
-        np.savetxt(os.path.join(self.assessor_save_dir, self.file_name_pre + "_val_loss_history.txt"), self.val_losses, delimiter=",")
-        np.savetxt(os.path.join(self.assessor_save_dir, self.file_name_pre + "_val_acc_history.txt"), self.val_accuracies, delimiter=",")
+        print("Saving history in", self.this_assessor_save_dir)
+        np.savetxt(os.path.join(self.this_assessor_save_dir, self.file_name_pre + "_loss_history.txt"), self.train_losses, delimiter=",")
+        np.savetxt(os.path.join(self.this_assessor_save_dir, self.file_name_pre + "_acc_history.txt"), self.train_accuracies, delimiter=",")
+        np.savetxt(os.path.join(self.this_assessor_save_dir, self.file_name_pre + "_val_loss_history.txt"), self.val_losses, delimiter=",")
+        np.savetxt(os.path.join(self.this_assessor_save_dir, self.file_name_pre + "_val_acc_history.txt"), self.val_accuracies, delimiter=",")
 
     # Plot and save losses and accuracies
     def plot_and_save_losses_and_accuracies(self, epoch):
         print("Saving plot for epoch", str(epoch), ":",
-            os.path.join(self.assessor_save_dir, self.file_name_pre + "_plots.png"))
+            os.path.join(self.this_assessor_save_dir, self.file_name_pre + "_plots.png"))
 
         plt.subplot(121)
         plt.plot(self.train_losses, label='train_loss')
@@ -230,7 +364,7 @@ class CheckpointAndMakePlots(Callback):
         plt.tight_layout()
         # plt.subplots_adjust(top=0.85)
         plt.suptitle(self.file_name_pre, fontsize=10)
-        plt.savefig(os.path.join(self.assessor_save_dir,
+        plt.savefig(os.path.join(self.this_assessor_save_dir,
                                  self.file_name_pre + "_plots_loss_acc.png"))
         plt.close()
 
@@ -262,6 +396,7 @@ def write_model_architecture(model, file_type='json', file_name="model"):
 
 def read_my_model(model_file_name="model.json", weights_file_name=None):
     # Load model
+    print("Loading model from", model_file_name, "...")
     # json
     if model_file_name.split('.')[-1] == 'json':
         with open(model_file_name, 'r') as json_file:
@@ -276,7 +411,9 @@ def read_my_model(model_file_name="model.json", weights_file_name=None):
         print("file_type can only be 'json' or 'yaml'")
     # Load weights
     if weights_file_name is not None:
+        print("Loading weights from", weights_file_name, "...")
         loaded_model.load_weights(weights_file_name)
     # Return
+    print("Done loading model.")
     return loaded_model
 
