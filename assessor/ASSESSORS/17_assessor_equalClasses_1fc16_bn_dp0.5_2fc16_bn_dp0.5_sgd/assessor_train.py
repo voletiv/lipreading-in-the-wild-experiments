e54db92c8b8d1fc34bc94af6a4318af6414f1d56
Vikram Voleti <vikky2904@gmail.com>
2017-11-24 15:42:18 +0530

    Results

---
+++ assessor/ASSESSORS/17_assessor_equalClasses_1fc16_bn_dp0.5_2fc16_bn_dp0.5_sgd/assessor_train.py
@@ -0,0 +1,94 @@
+import os
+
+from assessor_functions import *
+from assessor_model import *
+from assessor_train_params import *
+
+
+######################################################
+# DIR, PARAMS
+######################################################
+
+# Make the dir if it doesn't exist
+if not os.path.exists(this_assessor_save_dir):
+    print("Making dir", this_assessor_save_dir)
+    os.makedirs(this_assessor_save_dir)
+
+# Copy assessor_model file into this_assessor_save_dir
+os.system("cp assessor_model.py " + this_assessor_save_dir)
+print("Copied assessor_model.py to", this_assessor_save_dir)
+
+# Copy assessor_train_params file into this_assessor_save_dir
+os.system("cp assessor_train_params.py " + this_assessor_save_dir)
+print("Copied assessor_train_params.py to", this_assessor_save_dir)
+
+# Copy assessor_train file into this_assessor_save_dir
+os.system("cp assessor_train.py " + this_assessor_save_dir)
+print("Copied assessor_train.py to", this_assessor_save_dir)
+
+######################################################
+# GEN BATCHES OF IMAGES
+######################################################
+
+train_generator = generate_assessor_data_batches(batch_size=batch_size, data_dir=data_dir, collect_type=train_collect_type,
+                                                 shuffle=shuffle, equal_classes=equal_classes, use_CNN_LSTM=use_CNN_LSTM,
+                                                 grayscale_images=grayscale_images, random_crop=random_crop, random_flip=random_flip, verbose=verbose)
+
+val_generator = generate_assessor_data_batches(batch_size=batch_size, data_dir=data_dir, collect_type=val_collect_type,
+                                               shuffle=shuffle, equal_classes=equal_classes, use_CNN_LSTM=use_CNN_LSTM,
+                                               grayscale_images=grayscale_images, random_crop=False, random_flip=False, verbose=verbose)
+
+######################################################
+# MAKE MODEL
+######################################################
+
+assessor = my_assessor_model(use_CNN_LSTM=use_CNN_LSTM, mouth_nn=mouth_nn,
+                             conv_f_1=conv_f_1, conv_f_2=conv_f_2, conv_f_3=conv_f_3,
+                             mouth_features_dim=mouth_features_dim, lstm_units_1=lstm_units_1,
+                             dense_fc_1=dense_fc_1, dense_fc_2=dense_fc_2,
+                             grayscale_images=grayscale_images)
+
+assessor.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
+
+write_model_architecture(assessor, file_type='json', file_name=os.path.join(this_assessor_save_dir, this_assessor_model))
+write_model_architecture(assessor, file_type='yaml', file_name=os.path.join(this_assessor_save_dir, this_assessor_model))
+
+######################################################
+# CALLBACKS
+######################################################
+
+lr_reducer = ReduceLROnPlateau(factor=np.sqrt(0.1), cooldown=0, patience=10, min_lr=0.5e-6)
+
+early_stopper = EarlyStopping(min_delta=0.001, patience=50)
+
+checkpointAndMakePlots = CheckpointAndMakePlots(file_name_pre=this_assessor_model, this_assessor_save_dir=this_assessor_save_dir)
+
+######################################################
+# TRAIN
+######################################################
+
+saved_final_model = False
+
+try:
+    assessor.fit_generator(train_generator,
+                           steps_per_epoch=train_steps_per_epoch,
+                           # steps_per_epoch=1,
+                           epochs=n_epochs,
+                           callbacks=[lr_reducer, early_stopper, checkpointAndMakePlots],
+                           validation_data=val_generator,
+                           validation_steps=val_steps_per_epoch,
+                           # validation_steps=1,
+                           class_weight=class_weight,
+                           initial_epoch=0)
+
+except KeyboardInterrupt:
+    print("Saving latest weights as", os.path.join(this_assessor_save_dir, this_assessor_model+"_assessor.hdf5"), "...")
+    assessor.save_weights(os.path.join(this_assessor_save_dir, this_assessor_model+"_assessor.hdf5"))
+    saved_final_model = True
+
+if not saved_final_model:
+    print("Saving latest weights as", os.path.join(this_assessor_save_dir, this_assessor_model+"_assessor.hdf5"), "...")
+    assessor.save_weights(os.path.join(this_assessor_save_dir, this_assessor_model+"_assessor.hdf5"))
+    saved_final_model = True
+
+print("Done.")
---
+++ assessor/ASSESSORS/17_assessor_equalClasses_1fc16_bn_dp0.5_2fc16_bn_dp0.5_sgd/assessor_train_params.py
@@ -0,0 +1,115 @@
+import os
+
+from keras.optimizers import SGD
+from keras.callbacks import ReduceLROnPlateau, CSVLogger, EarlyStopping
+
+from assessor_functions import *
+from assessor_params import *
+
+######################################################
+# EXPERIMENT NUMBER
+######################################################
+
+prev_experiment_number = sorted([int(d.split('/')[-2].split('_')[0]) for d in glob.glob(os.path.join(ASSESSOR_SAVE_DIR, "[0-9]*/"))])[-1]
+
+experiment_number = prev_experiment_number + 1
+print("Experiment number:", experiment_number)
+
+
+######################################################
+# PARAMS
+######################################################
+
+# BATCH SIZE
+batch_size = 250
+
+# Data
+data_dir = LRW_DATA_DIR
+
+# Train
+train_collect_type = "val"
+
+# Val
+val_collect_type = "test"
+
+# Training
+shuffle = True
+equal_classes = True
+grayscale_images=True
+random_crop = True
+random_flip=True
+verbose = False
+
+# Assessor
+use_CNN_LSTM = False
+mouth_nn = 'flatten'    # doesn't matter
+conv_f_1 = 16   # doesn't matter
+conv_f_2 = 8    # doesn't matter
+conv_f_3 = 4    # doesn't matter
+mouth_features_dim = 512    # doesn't matter
+lstm_units_1 = 4    # doesn't matter
+dense_fc_1 = 16
+dense_fc_2 = 16
+dropout_p = 0.5
+
+# Compile
+optimizer = 'sgd'
+loss = 'binary_crossentropy'
+
+# Train
+train_lrw_word_set_num_txt_file_names = read_lrw_word_set_num_file_names(collect_type=train_collect_type, collect_by='sample')
+train_steps_per_epoch = len(train_lrw_word_set_num_txt_file_names) // batch_size
+# train_steps_per_epoch = 20     # Set less value so as not to take too much time computing on full train set
+
+n_epochs = 200
+
+# Val
+val_lrw_word_set_num_txt_file_names = read_lrw_word_set_num_file_names(collect_type=val_collect_type, collect_by='sample')
+# val_steps_per_epoch = len(val_lrw_word_set_num_txt_file_names) // batch_size
+val_steps_per_epoch = train_steps_per_epoch     # Set less value so as not to take too much time computing on full val set
+
+# Class weights
+# The lipreader is correct 70% of the time
+if equal_classes:
+    class_weight = None
+else:
+    class_weight = {0: .3, 1: .7}
+
+######################################################
+# THIS MODEL
+######################################################
+
+# THIS MODEL NAME
+this_assessor_model = str(experiment_number) + "_assessor"
+
+if equal_classes:
+    this_assessor_model += "_equalClasses"
+
+if use_CNN_LSTM:
+    if grayscale_images:
+        this_assessor_model += "_grayscaleImages"
+
+    this_assessor_model += "_" + mouth_nn
+
+    if mouth_nn == 'cnn':
+        this_assessor_model += '_1conv' + str(conv_f_1) + '_2conv' + str(conv_f_2) + '_3conv' + str(conv_f_3) + "_mouth" + str(mouth_features_dim)
+
+    elif 'resnet' in mouth_nn:
+        this_assessor_model += "_mouth" + str(mouth_features_dim)
+
+    this_assessor_model += "_lstm" + str(lstm_units_1)
+
+this_assessor_model += "_1fc" + str(dense_fc_1) + "_bn_dp" + str(dropout_p) + "_2fc" + str(dense_fc_2) + "_bn_dp" + str(dropout_p) + "_" + optimizer
+
+print("this_assessor_model:", this_assessor_model)
+
+# Save
+this_assessor_save_dir = os.path.realpath(os.path.join(ASSESSOR_SAVE_DIR, this_assessor_model))
+print("this_assessor_save_dir:", this_assessor_save_dir)
+
+######################################################
+# SGD
+######################################################
+
+if optimizer == 'sgd':
+    optimizer = SGD(lr=0.01, momentum=0.5)
