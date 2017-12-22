5f037af0f62d6ff037c95b5caf0449e53efcdcb7
Vikram Voleti <vikky2904@gmail.com>
2017-12-23 05:23:56 +0530

    LRW_train

--- assessor/assessor_evaluation.py
+++ assessor/assessor_evaluation.py
@@ -23,7 +23,8 @@ def load_preds_for_CNN_assessor(experiment_number, assessor=None, load_best_or_l
 
 def evaluate_assessor(lrw_val_assessor_preds=None, lrw_test_assessor_preds=None, assessor='CNN',
                       assessor_save_dir='/shared/fusor/home/voleti.vikram/ASSESSORS/GBT_full_testSplit0.1/', assessor_threshold=0.5,
-                      lipreader_lrw_val_softmax=None, lipreader_lrw_test_softmax=None, lrw_correct_one_hot_y_arg=None):
+                      lipreader_lrw_val_softmax=None, lipreader_lrw_test_softmax=None,
+                      lrw_correct_one_hot_y_arg_val=None, lrw_correct_one_hot_y_arg_test=None):
 
     # lrw_assessor_preds is an nx1 probability
 
@@ -40,35 +41,49 @@ def evaluate_assessor(lrw_val_assessor_preds=None, lrw_test_assessor_preds=None,
                 return
 
     # SOFTMAX, CORRECT_ONE_HOT_Y_ARG
-    if lipreader_lrw_val_softmax is None or lipreader_lrw_test_softmax is None or lrw_correct_one_hot_y_arg is None:
+    if lipreader_lrw_val_softmax is None or lipreader_lrw_test_softmax is None or lrw_correct_one_hot_y_arg_val is None or lrw_correct_one_hot_y_arg_test is None:
         print("Loading softmax, correct_one_hot_y_arg...")
         try:
-            _, lipreader_lrw_val_softmax, lrw_correct_one_hot_y_arg = load_dense_softmax_y(collect_type="val")
-            _, lipreader_lrw_test_softmax, lrw_correct_one_hot_y_arg = load_dense_softmax_y(collect_type="test")
+            _, lipreader_lrw_train_softmax, lrw_correct_one_hot_y_arg_train = load_dense_softmax_y(collect_type="train")
+            _, lipreader_lrw_val_softmax, lrw_correct_one_hot_y_arg_val = load_dense_softmax_y(collect_type="val")
+            _, lipreader_lrw_test_softmax, lrw_correct_one_hot_y_arg_test = load_dense_softmax_y(collect_type="test")
+            lipreader_lrw_val_softmax = np.vstack((lipreader_lrw_train_softmax, lipreader_lrw_val_softmax))
+            lrw_correct_one_hot_y_arg_val = np.append(lrw_correct_one_hot_y_arg_train, lrw_correct_one_hot_y_arg_val)
         except OSError as err:
             print(err)
             return
 
+    total_samples_val = len(lipreader_lrw_val_softmax)
+    samples_per_word_val = total_samples_val // 500
+    total_samples_test = len(lipreader_lrw_test_softmax)
+    samples_per_word_test = total_samples_test // 500
+    correct_args_val = np.zeros((total_samples_val))
+    correct_args_test = np.zeros((total_samples_test))
+    for w in range(500):
+        correct_args_val[w*samples_per_word_val:(w+1)*samples_per_word_val] = lrw_correct_one_hot_y_arg_val[w*50]
+        correct_args_test[w*samples_per_word_test:(w+1)*samples_per_word_test] = lrw_correct_one_hot_y_arg_test[w*50]
+
+
     # CORRECT_OR_WRONG
-    lipreader_lrw_val_correct_or_wrong = np.argmax(lipreader_lrw_val_softmax, axis=1) == lrw_correct_one_hot_y_arg
-    lipreader_lrw_test_correct_or_wrong = np.argmax(lipreader_lrw_test_softmax, axis=1) == lrw_correct_one_hot_y_arg
+    lipreader_lrw_val_correct_or_wrong = np.argmax(lipreader_lrw_val_softmax, axis=1) == correct_args_val
+    lipreader_lrw_test_correct_or_wrong = np.argmax(lipreader_lrw_test_softmax, axis=1) == correct_args_test
 
     # Evaluate and plot ROC with Operating Point
     print("Evaluating and plot ROC with Operating Point...")
-    evaluate_and_plot_ROC_with_OP(lipreader_lrw_val_correct_or_wrong, lrw_val_assessor_preds, collect_type="val",
+    evaluate_and_plot_ROC_with_OP(lipreader_lrw_val_correct_or_wrong, lrw_val_assessor_preds, collect_type="train",
                                   assessor_threshold=assessor_threshold, save_and_close=False)
     evaluate_and_plot_ROC_with_OP(lipreader_lrw_test_correct_or_wrong, lrw_test_assessor_preds, collect_type="test",
                                   assessor_threshold=assessor_threshold, save_and_close=True)
 
     # Evaluate average precision, plot PR curve
     print("Evaluatinging average precision, plot PR curve...")
-    evaluate_avg_precision_plot_PR_curve(lipreader_lrw_val_correct_or_wrong, lrw_val_assessor_preds, collect_type="val", save_and_close=False)
+    evaluate_avg_precision_plot_PR_curve(lipreader_lrw_val_correct_or_wrong, lrw_val_assessor_preds, collect_type="train", save_and_close=False)
     evaluate_avg_precision_plot_PR_curve(lipreader_lrw_test_correct_or_wrong, lrw_test_assessor_preds, collect_type="test", save_and_close=True)
 
     # Compare precision, recall of lipreader results and assessor-filtered results
     print("Comparing precision, recall of lipreader results and assessor-filtered results...")
-    compare_PR_of_lipreader_and_assessor(lipreader_lrw_val_softmax, lrw_correct_one_hot_y_arg, lrw_val_assessor_preds, collect_type="val", assessor_threshold=assessor_threshold)
-    compare_PR_of_lipreader_and_assessor(lipreader_lrw_test_softmax, lrw_correct_one_hot_y_arg, lrw_test_assessor_preds, collect_type="test", assessor_threshold=assessor_threshold)
+    compare_PR_of_lipreader_and_assessor(lipreader_lrw_val_softmax, correct_args_val, lrw_val_assessor_preds, collect_type="train", assessor_threshold=assessor_threshold)
+    compare_PR_of_lipreader_and_assessor(lipreader_lrw_test_softmax, correct_args_test, lrw_test_assessor_preds, collect_type="test", assessor_threshold=assessor_threshold)
 
     print("Done.")
 
