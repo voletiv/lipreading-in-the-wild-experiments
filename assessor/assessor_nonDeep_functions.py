aab496635533712bb831f43922e26b258d30e796
Vikram Voleti <vikky2904@gmail.com>
2018-01-30 14:19:31 +0530

    get_spl_lrw_data

--- assessor/assessor_nonDeep_functions.py
+++ assessor/assessor_nonDeep_functions.py
@@ -250,6 +250,165 @@ def get_lrw_data(use_syncnet_preds=True, use_pca=False, use_dense=True, use_soft
     return X_train, y_train, X_test, y_test, lipreader_lrw_val_softmax, lipreader_lrw_test_softmax, lrw_correct_one_hot_y_arg_val, lrw_correct_one_hot_y_arg_test
 
 
+
+def get_spl_lrw_data(use_syncnet_preds=True, use_pca=False, use_dense=True, use_softmax=True, use_softmax_ratios=True, mix=None, test_number_of_words=114):
+
+    lrw_word_set_num_txt_file_names_LRW_train = read_lrw_word_set_num_file_names(collect_type='train', collect_by='vocab_word')
+    lrw_word_set_num_txt_file_names_LRW_val = read_lrw_word_set_num_file_names(collect_type='val', collect_by='vocab_word')
+    num_of_samples_per_word_LRW_train = []
+    num_of_samples_per_word_LRW_val = []
+    for w in range(500):
+        num_of_samples_per_word_LRW_train.append(len(lrw_word_set_num_txt_file_names_LRW_train[w]))
+        num_of_samples_per_word_LRW_val.append(len(lrw_word_set_num_txt_file_names_LRW_val[w]))
+
+    # Number of frames
+    lrw_train_n_of_frames = load_array_of_var_per_sample_from_csv(csv_file_name=N_OF_FRAMES_PER_SAMPLE_CSV_FILE, collect_type='train', collect_by='vocab_word')
+    lrw_val_n_of_frames = load_array_of_var_per_sample_from_csv(csv_file_name=N_OF_FRAMES_PER_SAMPLE_CSV_FILE, collect_type='val', collect_by='sample')
+    lrw_test_n_of_frames = load_array_of_var_per_sample_from_csv(csv_file_name=N_OF_FRAMES_PER_SAMPLE_CSV_FILE, collect_type='test', collect_by='sample')
+
+    lrw_train_n_of_frames_by_word = load_array_of_var_per_sample_from_csv(csv_file_name=N_OF_FRAMES_PER_SAMPLE_CSV_FILE, collect_type='train', collect_by='vocab_word')
+    lrw_train_n_of_frames = []
+    for w in range(500):
+        for i in range(train_samples_per_word):
+            lrw_train_n_of_frames.append(lrw_train_n_of_frames_by_word[w][i])
+
+    if use_syncnet_preds:
+        # Syncnet preds
+        lrw_val_syncnet_preds_full = load_syncnet_preds(collect_type='val')
+        lrw_test_syncnet_preds_full = load_syncnet_preds(collect_type='test')
+        if use_LRW_train:
+            lrw_train_syncnet_preds_full = load_syncnet_preds(collect_type='train')
+
+        lrw_val_syncnet_preds = np.zeros((lrw_val_syncnet_preds_full.shape[0], lrw_val_syncnet_preds_full.shape[2]))
+        lrw_test_syncnet_preds = np.zeros((lrw_test_syncnet_preds_full.shape[0], lrw_test_syncnet_preds_full.shape[2]))
+        if use_LRW_train:
+            lrw_train_syncnet_preds = np.zeros((lrw_train_syncnet_preds_full.shape[0], lrw_train_syncnet_preds_full.shape[2]))
+
+        for i in range(len(lrw_val_syncnet_preds)):
+            lrw_val_syncnet_preds[i] = lrw_val_syncnet_preds_full[i][-(lrw_val_n_of_frames[i]//2 + 1)]
+
+        for i in range(len(lrw_test_syncnet_preds)):
+            lrw_test_syncnet_preds[i] = lrw_test_syncnet_preds_full[i][-(lrw_test_n_of_frames[i]//2 + 1)]
+
+        if use_LRW_train:
+            for i in range(len(lrw_train_syncnet_preds)):
+                lrw_train_syncnet_preds[i] = lrw_train_syncnet_preds_full[i][-(lrw_train_n_of_frames[i]//2 + 1)]
+
+    # Dense, Softmax, One_hot_y_arg
+    lipreader_lrw_val_dense, lipreader_lrw_val_softmax, lrw_correct_one_hot_y_arg_val = load_dense_softmax_y(collect_type="val")
+    lipreader_lrw_test_dense, lipreader_lrw_test_softmax, lrw_correct_one_hot_y_arg_test = load_dense_softmax_y(collect_type="test")
+    if use_LRW_train:
+        print("Loading lipreader_lrw_train_dense_softmax_y...")
+        lipreader_lrw_train_dense, lipreader_lrw_train_softmax, lrw_correct_one_hot_y_arg_train = load_dense_softmax_y(collect_type="train")
+        print("Loaded")
+
+    lipreader_lrw_val_correct_or_wrong = np.argmax(lipreader_lrw_val_softmax, axis=1) == lrw_correct_one_hot_y_arg_val
+    lipreader_lrw_test_correct_or_wrong = np.argmax(lipreader_lrw_test_softmax, axis=1) == lrw_correct_one_hot_y_arg_test
+    if use_LRW_train:
+        lipreader_lrw_train_correct_or_wrong = np.argmax(lipreader_lrw_train_softmax, axis=1) == lrw_correct_one_hot_y_arg_train
+
+    lipreader_lrw_val_softmax_ratios = load_softmax_ratios(collect_type="val")
+    lipreader_lrw_test_softmax_ratios = load_softmax_ratios(collect_type="test")
+    if use_LRW_train:
+        lipreader_lrw_train_softmax_ratios = load_softmax_ratios(collect_type="train")
+
+    if use_LRW_train:
+        lrw_val_n_of_frames = lrw_train_n_of_frames + lrw_val_n_of_frames
+        lrw_val_syncnet_preds = np.vstack((lrw_train_syncnet_preds, lrw_val_syncnet_preds))
+        lipreader_lrw_val_dense = np.vstack((lipreader_lrw_train_dense, lipreader_lrw_val_dense))
+        lipreader_lrw_val_softmax = np.vstack((lipreader_lrw_train_softmax, lipreader_lrw_val_softmax))
+        lipreader_lrw_val_correct_or_wrong = np.append(lipreader_lrw_train_correct_or_wrong, lipreader_lrw_val_correct_or_wrong)
+        lrw_correct_one_hot_y_arg_val = np.append(lrw_correct_one_hot_y_arg_train, lrw_correct_one_hot_y_arg_val)
+        lipreader_lrw_val_softmax_ratios = np.vstack((lipreader_lrw_train_softmax_ratios, lipreader_lrw_val_softmax_ratios))
+
+    if use_pca:
+        # PCA
+        pca_dense = decomposition.PCA(n_components=32)
+        pca_dense.fit(lipreader_lrw_val_dense)
+        lipreader_lrw_val_dense = pca_dense.transform(lipreader_lrw_val_dense)
+        lipreader_lrw_test_dense = pca_dense.transform(lipreader_lrw_test_dense)
+        pca_softmax = decomposition.PCA(n_components=32)
+        pca_softmax.fit(lipreader_lrw_val_softmax)
+        lipreader_lrw_val_softmax = pca_softmax.transform(lipreader_lrw_val_softmax)
+        lipreader_lrw_test_softmax = pca_softmax.transform(lipreader_lrw_test_softmax)
+
+    X_train = np.reshape(lrw_val_n_of_frames, (len(lrw_val_n_of_frames), 1))
+    y_train = lipreader_lrw_val_correct_or_wrong
+    X_test = np.reshape(lrw_test_n_of_frames, (len(lrw_test_n_of_frames), 1))
+    y_test = lipreader_lrw_test_correct_or_wrong
+
+    if use_syncnet_preds:
+        X_train = np.hstack((lrw_val_syncnet_preds, X_train))
+        X_test = np.hstack((lrw_test_syncnet_preds, X_test))
+
+    if use_dense:
+        X_train = np.hstack((X_train, lipreader_lrw_val_dense))
+        X_test = np.hstack((X_test, lipreader_lrw_test_dense))
+
+    if use_softmax:
+        X_train = np.hstack((X_train, lipreader_lrw_val_softmax))
+        X_test = np.hstack((X_test, lipreader_lrw_test_softmax))
+
+    if use_softmax_ratios:
+        X_train = np.hstack((X_train, lipreader_lrw_val_softmax_ratios))
+        X_test = np.hstack((X_test, lipreader_lrw_test_softmax_ratios))
+
+    # # Choose 50-50
+    # pos_X = train_features[np.where(lipreader_lrw_val_correct_or_wrong == 1)]
+    # pos_y = lipreader_lrw_val_correct_or_wrong[np.where(lipreader_lrw_val_correct_or_wrong == 1)]
+    # neg_X = train_features[np.where(lipreader_lrw_val_correct_or_wrong == 0)]
+    # neg_y = lipreader_lrw_val_correct_or_wrong[np.where(lipreader_lrw_val_correct_or_wrong == 0)]
+    # X_train_eq = np.vstack((pos_X[:len(neg_X)], neg_X))
+    # y_train_eq = np.append(pos_y[:len(neg_X)], neg_y)
+    # X_train, X_val, y_train, y_val = train_test_split(X_train_eq, y_train_eq, test_size=0.33, random_state=0)
+
+    if mix is not None:
+        X_train_old = np.array(X_train)
+        y_train_old = np.array(y_train)
+        X_test_old = np.array(X_test)
+        y_test_old = np.array(y_test)
+        lipreader_lrw_val_softmax_old = np.array(lipreader_lrw_val_softmax)
+        lipreader_lrw_test_softmax_old = np.array(lipreader_lrw_test_softmax)
+
+        if mix == '2525':
+            print('25-25 mixing!')
+            for w in range(500):
+                X_train[w*50+25:(w+1)*50] = X_test_old[w*50+25:(w+1)*50]
+                X_test[w*50+25:(w+1)*50] = X_train_old[w*50+25:(w+1)*50]
+                lipreader_lrw_val_softmax[w*50+25:(w+1)*50] = lipreader_lrw_test_softmax_old[w*50+25:(w+1)*50]
+                lipreader_lrw_test_softmax[w*50+25:(w+1)*50] = lipreader_lrw_val_softmax_old[w*50+25:(w+1)*50]
+
+        elif mix == 'alternate':
+            print('alternate mixing!')
+            for w in range(500):
+                idx = range(w*50, (w+1)*50, 2)
+                X_train[idx] = X_test_old[idx]
+                X_test[idx] = X_train_old[idx]
+                lipreader_lrw_val_softmax[idx] = lipreader_lrw_test_softmax_old[idx]
+                lipreader_lrw_test_softmax[idx] = lipreader_lrw_val_softmax_old[idx]
+
+        elif mix == 'testFirst0.1':
+            print('testFirst0.1!')
+            X_train = np.zeros(((50+40)*500, X_train.shape[1]))
+            y_train = np.zeros(((50+40)*500))
+            X_test = np.zeros((10*500, X_test.shape[1]))
+            y_test = np.zeros((10*500))
+            lipreader_lrw_val_softmax = np.zeros(((50+40)*500, 500))
+            lipreader_lrw_test_softmax = np.zeros((10*500, 500))
+            for w in range(500):
+                X_train[(w*(50+40)):(w*(50+40)+50)] = X_train_old[(w*50):(w*50+50)]
+                X_train[(w*(50+40)+50):(w*(50+40)+(50+40))] = X_test_old[(w*50+10):(w*50+50)]
+                y_train[(w*(50+40)):(w*(50+40)+50)] = y_train_old[(w*50):(w*50+50)]
+                y_train[(w*(50+40)+50):(w*(50+40)+(50+40))] = y_test_old[(w*50+10):(w*50+50)]
+                X_test[(w*10):((w+1)*10)] = X_test_old[(w*50):(w*50+10)]
+                y_test[(w*10):((w+1)*10)] = y_test_old[(w*50):(w*50+10)]
+                lipreader_lrw_val_softmax[(w*(50+40)):(w*(50+40)+50)] = lipreader_lrw_val_softmax_old[(w*50):(w*50+50)]
+                lipreader_lrw_val_softmax[(w*(50+40)+50):(w*(50+40)+(50+40))] = lipreader_lrw_test_softmax_old[(w*50+10):(w*50+50)]
+                lipreader_lrw_test_softmax[(w*10):((w+1)*10)] = lipreader_lrw_test_softmax_old[(w*50):(w*50+10)]
+
+    return X_train, y_train, X_test, y_test, lipreader_lrw_val_softmax, lipreader_lrw_test_softmax, lrw_correct_one_hot_y_arg_val, lrw_correct_one_hot_y_arg_test
+
+
 #####################################
 # GBT, etc.
 #####################################
